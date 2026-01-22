e7dc3549cb2ba9ac51331f354561fc03bc56996c
Dushyant Kumar <149196558+dushyant1222@users.noreply.github.com>
2026-01-23 00:12:50 +0530

    debug

--- Frontend/contact-manager-frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-import { getAllContacts } from "./api/contactApi";
-import ContactsPage from "./pages/ContactsPage";
-
-function App() {
-  return <ContactsPage />;
-}
-
-export default App;
